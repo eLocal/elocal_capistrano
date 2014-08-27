34daf80be96b8fe80c4b609485c89d8009a0ebcc
Rob Di Marco <rob.dimarco@416software.com>
2014-08-27 16:49:20 -0400

    Add a task for symlinking shared configurations

---
+++ lib/elocal_capistrano/deploy.rb
@@ -0,0 +1,21 @@
+Capistrano::Configuration.instance.load do
+
+  namespace :deploy do
+
+    desc "Symlink configuration files to the config/ directory."
+    task :symlink_shared_configuration, :roles => :app do
+      run <<-CMD
+        if [ -d #{deploy_to}/shared/config ]; then
+          cd #{deploy_to}/shared/config;
+          for f in `ls -x`; do
+            if [ -f #{release_path}/config/$f ]; then
+              mv #{release_path}/config/$f #{release_path}/config/$f.orig;
+            fi;
+            ln -nfs #{deploy_to}/shared/config/$f #{release_path}/config/$f;
+          done;
+        fi;
+      CMD
+    end
+  end
+
+end
