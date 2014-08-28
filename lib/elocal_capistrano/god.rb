e7713013761f64651d853ebbef8d2832924c8715
Rob Di Marco <rob.dimarco@416software.com>
2014-08-28 09:12:10 -0400

    Use an explicit variable

--- lib/elocal_capistrano/god.rb
+++ lib/elocal_capistrano/god.rb
@@ -1,10 +1,9 @@
 Capistrano::Configuration.instance.load do
+  set(:god_application_name) { application }
   namespace :god do
-    namespace :application do
-      [:start, :stop, :restart].each do |t|
-        task t, :roles => :app, except: { no_release: true } do
-          sudo "god #{t} #{application}"
-        end
+    [:start, :stop, :restart].each do |t|
+      task t, :roles => :app, except: { no_release: true } do
+        sudo "god #{t} #{god_application_name}"
       end
     end
   end
